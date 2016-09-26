789b7e450eca0d2aa57d09131a3071afe1882589
brodenbrett <broden.d.brett@gmail.com>
2016-09-25 22:01:25 -0600

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,16 @@
 //Toggle menu nav
-var body = document.querySelector('body');
-var sideNav = document.querySelector('.side-nav');
-var icon = document.querySelector('.icon');
+var body = document.querySelector('body'); //webpage
+var sideNav = document.querySelector('.side-nav'); //sidebar
+var icon = document.querySelector('.icon'); //menu button
 
 function toggleNav(){
 	if(sideNav.style.width = '200px' && body.style.marginLeft == '200px'){		
-		sideNav.style.width = '0px';
-		body.style.marginLeft = '0px';
-		icon.classList.remove('rotate');
+		sideNav.style.width = '0px'; //slide out the nav
+		body.style.marginLeft = '0px'; //move the page into place
+		icon.classList.remove('rotate'); //rotate the icon upright
 	} else {
-		sideNav.style.width = '200px';
-		body.style.marginLeft = '200px';
-		icon.classList.add('rotate');
+		sideNav.style.width = '200px'; //slide in the nav
+		body.style.marginLeft = '200px'; //move the page over
+		icon.classList.add('rotate'); //rotate the icon sideways
 	}
 }
